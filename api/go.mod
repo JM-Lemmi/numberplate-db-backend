17b18731c7348a06c0b7ebb0391f96bb0aaa4746
Julian Lemmerich <s212689@student.dhbw-mannheim.de>
2022-07-15 14:09:38 +0000

    add working postgres-insert at /numberplates

--- api/go.mod
+++ api/go.mod
@@ -11,5 +11,6 @@ require (
 
 require (
 	github.com/daaku/go.zipexe v1.0.0 // indirect
+	github.com/lib/pq v1.10.6 // indirect
 	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
 )
--- api/handlers.go
+++ api/handlers.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"net/http"
+
+	_ "github.com/lib/pq"
 
 	"github.com/gorilla/mux"
 	log "github.com/sirupsen/logrus"
+)
+
+const (
+	host     = "localhost"
+	port     = 25432
+	user     = "postgres"
+	password = "password"
+	dbname   = "numberplates"
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
@@ -18,13 +30,34 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func numberplateHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
+	//vars := mux.Vars(r)
 	requestLogger := log.WithFields(log.Fields{"client": GetIP(r)})
 	requestLogger.Infoln("New Request!")
 
+	// from https://golangdocs.com/golang-postgresql-example
+	// connection string
+	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	// open database
+	db, err := sql.Open("postgres", psqlconn)
+	CheckError(err)
+
+	// close database
+	defer db.Close()
+
+	// check db
+	err = db.Ping()
+	CheckError(err)
+
+	requestLogger.Infoln("PostgreSQL connected!")
+
+	//from https://golangdocs.com/golang-postgresql-example
+	insertDynStmt := `INSERT INTO "numberplates"("plate", "owner") values($1, $2)`
+	_, e := db.Exec(insertDynStmt, "HGJL1999", "Julian Lemmerich")
+	CheckError(e)
+
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Hello World!"))
+	w.Write([]byte("check console"))
 }
 
 func GetIP(r *http.Request) string {
@@ -33,4 +66,10 @@ func GetIP(r *http.Request) string {
 		return forwarded
 	}
 	return r.RemoteAddr
+}
+
+func CheckError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
